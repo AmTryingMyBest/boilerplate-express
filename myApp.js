9e04af99bd7111cea5ba17ba5abfa9960e9b23e6
AmTryingMyBest <w.obadha@gmail.com>
2023-03-04 11:04:26 +0300

    respond to request with a file

--- myApp.js
+++ myApp.js
@@ -2,8 +2,10 @@ let express = require("express");
 const res = require("express/lib/response");
 let app = express();
 
+absolutePath = __dirname + "/views/index.html";
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  // res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
 
 module.exports = app;
