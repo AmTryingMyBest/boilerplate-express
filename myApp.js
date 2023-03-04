212c6c21086831b76da778338d74738912d9f041
AmTryingMyBest <w.obadha@gmail.com>
2023-03-04 08:22:20 +0300

    starting a working express server

--- myApp.js
+++ myApp.js
@@ -1,39 +1,9 @@
-let express = require('express');
+let express = require("express");
+const res = require("express/lib/response");
 let app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
