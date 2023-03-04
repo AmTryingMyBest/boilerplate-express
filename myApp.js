3f157023e6787ab78d1c4aba6b730294a0f21169
AmTryingMyBest <w.obadha@gmail.com>
2023-03-04 16:29:00 +0300

    serve CSS stylesheet

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ let express = require("express");
 const res = require("express/lib/response");
 let app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 absolutePath = __dirname + "/views/index.html";
 app.get("/", (req, res) => {
   // res.send("Hello Express");
