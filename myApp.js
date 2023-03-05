2982b16fa4f0aff8e688e4cf0f169aa50749c84d
AmTryingMyBest <w.obadha@gmail.com>
2023-03-05 12:26:43 +0300

    see if this passes the test

--- myApp.js
+++ myApp.js
@@ -5,10 +5,10 @@ let app = express();
 app.use(express.static(__dirname + "/public"));
 
 absolutePath = __dirname + "/views/index.html";
-app.get("/", (req, res) => {
-  // res.send("Hello Express");
-  res.sendFile(absolutePath);
-});
+// app.get("/", (req, res) => {
+//   // res.send("Hello Express");
+//   res.sendFile(absolutePath);
+// });
 
 app.get("/json", (req, res) => {
   res.json({ message: "Hello json" });
