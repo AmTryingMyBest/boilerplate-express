16fd308b74cc538d58d72d9218f83fd3f11af038
AmTryingMyBest <w.obadha@gmail.com>
2023-03-05 16:08:52 +0300

    use a .env file

--- myApp.js
+++ myApp.js
@@ -1,16 +1,21 @@
 let express = require("express");
 const res = require("express/lib/response");
 let app = express();
+require("dotenv").config();
 
 app.use(express.static(__dirname + "/public"));
 
-// absolutePath = __dirname + "/views/index.html";
-// app.get("/", (req, res) => {
-//   // res.send("Hello Express");
-//   res.sendFile(absolutePath);
-// });
+absolutePath = __dirname + "/views/index.html";
+app.get("/", (req, res) => {
+  // res.send("Hello Express");
+  res.sendFile(absolutePath);
+});
 
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
+  if ((process.env.MESSAGE_STYLE = "uppercase")) {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
 });
 module.exports = app;
